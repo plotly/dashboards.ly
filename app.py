57574c8fcf3e0447fea57042ab26ac0890543ac4
chriddyp <chris@plot.ly>
2015-09-16 13:07:53 -0400

    force cache resets until random bundles are used

--- app.py
+++ app.py
@@ -69,5 +69,15 @@ def view():
     return render_template('base.html', mode='view')
 
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
