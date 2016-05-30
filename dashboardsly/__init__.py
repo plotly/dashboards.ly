566b1a977af14bb532e7bbc4cdc84600f0a9e74e
Miklós Tusz <mdtusz@gmail.com>
2016-05-30 12:19:28 -0400

    Reorganize views and add gzip compression

--- dashboardsly/__init__.py
+++ dashboardsly/__init__.py
@@ -1,7 +1,44 @@
-from flask import Flask
 import os
+import sys
+import logging
+
+from flask import Flask
+from flask.ext import assets
+from flask.ext.compress import Compress
+from flask.ext.cors import CORS
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from flask_httpauth import HTTPBasicAuth
+from flask_reverse_proxy import FlaskReverseProxied
+from flask_sslify import SSLify
+
 
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+
+Compress(app)
+CORS(app)
+FlaskReverseProxied(app)
+
+
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+
+env = assets.Environment(app)
+env.load_path = [os.path.join(os.path.dirname(__file__), 'sass')]
+env.register(
+    'css_all',
+    assets.Bundle(
+        'skeleton.scss',
+        filters='scss',
+        output='css_all.css'
+    )
+)
+
 
 if 'DYNO' in os.environ or 'DASHBOARDSLY_PROD' in os.environ:
     # production on Heroku
@@ -12,11 +49,20 @@ else:
     # development on localhost
     app.config.from_object('dashboardsly.config.DevelopmentConfig')
 
+
 if app.config['PLOTLY_ON_PREM']:
     app.config['DEFAULT_USERNAME'] = None
     app.config['DEFAULT_APIKEY'] = None
+
+    if app.config['SSL_ENABLED']:
+        # On-Prem, SSL enabled: temporary redirects, and disable HSTS
+        # by setting the age to 60s.  (Allows SSL to be switched off.)
+        SSLify(app, permanent=False, age=60)
 else:
     app.config['DEFAULT_USERNAME'] = 'benji.b'
     app.config['DEFAULT_APIKEY'] = 'op16fm0vke'
+    # Non-Prem: always enable SSL with permanent redirects and HSTS
+    SSLify(app, permanent=True)
+
 
 import dashboardsly.views
--- dashboardsly/config.py
+++ dashboardsly/config.py
@@ -1,21 +1,19 @@
-
-from os.path import abspath, dirname
 import os
-
-_cwd = dirname(abspath(__file__))
 
 
 class Config(object):
     PLOTLY_API_DOMAIN = os.environ.get(
-        "PLOTLY_API_DOMAIN", "https://api.plot.ly")
+        'PLOTLY_API_DOMAIN', 'https://api.plot.ly')
     PLOTLY_DOMAIN = os.environ.get(
-        "PLOTLY_DOMAIN", "https://plot.ly")
+        'PLOTLY_DOMAIN', 'https://plot.ly')
     PLOTLY_DOMAIN_EXT = os.environ.get(
-        "PLOTLY_DOMAIN_EXT", PLOTLY_DOMAIN)
+        'PLOTLY_DOMAIN_EXT', PLOTLY_DOMAIN)
     USE_CONTENT_DELIVERY_NETWORKS = bool(int(os.environ.get(
-        "PLOTLY_USE_CONTENT_DELIVERY_NETWORKS", 1)))
-    PLOTLY_ON_PREM = bool(int(os.environ.get("PLOTLY_ON_PREM", 0)))
-    SSL_ENABLED = bool(int(os.getenv('PLOTLY_DASHBOARDSLY_SSL_ENABLED', 1)))
+        'PLOTLY_USE_CONTENT_DELIVERY_NETWORKS', 1)))
+    PLOTLY_ON_PREM = bool(int(os.environ.get(
+        'PLOTLY_ON_PREM', 0)))
+    SSL_ENABLED = bool(int(os.environ.get(
+        'PLOTLY_DASHBOARDSLY_SSL_ENABLED', 1)))
 
 
 class ProductionConfig(Config):
@@ -24,7 +22,6 @@ class ProductionConfig(Config):
     """
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')
-
     ENVIRONMENT = 'PROD'
 
 
@@ -33,14 +30,14 @@ class DevelopmentConfig(Config):
     localhost
     """
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://chriddyp@localhost/chriddyp'
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@192.168.99.100/postgres'
     ENVIRONMENT = 'DEV'
 
 
 class TestingConfig(DevelopmentConfig):
     import warnings
-    warnings.filterwarnings("ignore")
+    warnings.filterwarnings('ignore')
     # http://stackoverflow.com/questions/26647032/py-test-to-test-flask-register-assertionerror-popped-wrong-request-context
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     TESTING = True
+    ENVIRONMENT = 'TEST'
